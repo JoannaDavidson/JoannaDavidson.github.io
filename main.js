98dad922aea8d2f9d33971c61d1b0b3efe914255
Joanna Davidson <joanna@shrikesong.com>
2018-09-28 01:48:19 -0400

    Major reworking. Moved to single page with JS.

---
+++ main.js
@@ -0,0 +1,24 @@
+var aboutMe = document.getElementById("page-container__about")
+var portfolio = document.getElementById("page-container__portfolio")
+var resume = document.getElementById("page-container__resume")
+
+function aboutMeUnhide() {
+    aboutMe.classList.remove("hide");
+    aboutMe.classList.add("content_section");
+    portfolio.classList.add("hide");
+    resume.classList.add("hide");
+}
+
+function portfolioUnhide() {
+    portfolio.classList.remove("hide");
+    portfolio.classList.add("content_section");
+    aboutMe.classList.add("hide");
+    resume.classList.add("hide");
+}
+
+function resumeUnhide() {
+    resume.classList.remove("hide");
+    resume.classList.add("content_section");
+    portfolio.classList.add("hide");
+    aboutMe.classList.add("hide");
+}
